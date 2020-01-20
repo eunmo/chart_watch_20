b64c2d40c838ca23c6318d0b5751f113265d714a
eunmo <eunmo@cs.stanford.edu>
2020-01-20 14:58:05 +0000

    lint

--- server/chart_watch.js
+++ server/chart_watch.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3010
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = 3010;
 
-app.listen(port, () => console.log(`Chart Watch 20 App listening on port ${port}`));
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () =>
+  console.log(`Chart Watch 20 App listening on port ${port}`)
+);
