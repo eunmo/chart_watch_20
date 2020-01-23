4344ae2bcaccceefcfd96a1c1ba8393f0f77fcad
eunmo <eunmo@cs.stanford.edu>
2020-01-23 15:14:50 +0000

    complete albums

--- server/db/__tests__/albums.test.js
+++ server/db/__tests__/albums.test.js
@@ -1,7 +1,7 @@
 const Albums = require('../albums.js');
 
-const indexes = [1, 2, 3, 4];
-const albumIds = [1, 10, 30, 100];
+const indexes = [1, 2, 3, 4, 5];
+const albumIds = [1, 10, 25, 77, 100];
 const albumRows = new Map([
   [
     1,
@@ -24,13 +24,23 @@ const albumRows = new Map([
     }
   ],
   [
-    30,
+    25,
     {
-      id: 30,
-      title: 'K팝 스타 시즌4 `마음대로`',
+      id: 25,
+      title: 'K팝 스타 시즌4 - 캐스팅 오디션 Part 1',
       format: 'Single',
       format2: 'Soundtrack',
-      release: new Date('2014-12-14T00:00:00.000Z')
+      release: new Date('2015-01-18T00:00:00.000Z')
+    }
+  ],
+  [
+    77,
+    {
+      id: 77,
+      title: '소유X어반자카파 (권순일&박용인) `틈`',
+      format: 'Single',
+      format2: null,
+      release: new Date('2014-09-26T00:00:00.000Z')
     }
   ],
   [
@@ -45,11 +55,113 @@ const albumRows = new Map([
   ]
 ]);
 
+const albumArtists = new Map([
+  [1, [{ id: 1, name: 'Apink' }]],
+  [10, [{ id: 58, name: 'One Direction' }]],
+  [25, [{ id: 1301, name: 'K팝 스타' }]],
+  [
+    77,
+    [
+      {
+        id: 137,
+        name: '소유',
+        Bs: new Map([['m', { id: 136, name: '씨스타' }]])
+      },
+      {
+        id: 257,
+        name: '권순일',
+        Bs: new Map([['m', { id: 259, name: '어반 자카파' }]])
+      },
+      {
+        id: 258,
+        name: '박용인',
+        Bs: new Map([['m', { id: 259, name: '어반 자카파' }]])
+      }
+    ]
+  ],
+  [100, [{ id: 300, name: 'Rixton' }]]
+]);
+
+const albumSongs = new Map([
+  [
+    1,
+    [
+      { id: 1, disk: 1, track: 1 },
+      { id: 5, disk: 1, track: 2 },
+      { id: 4, disk: 1, track: 3 },
+      { id: 3, disk: 1, track: 4 },
+      { id: 2, disk: 1, track: 5 }
+    ]
+  ],
+  [
+    10,
+    [
+      { id: 78, disk: 1, track: 1 },
+      { id: 89, disk: 1, track: 2 },
+      { id: 88, disk: 1, track: 3 },
+      { id: 87, disk: 1, track: 4 },
+      { id: 86, disk: 1, track: 5 },
+      { id: 85, disk: 1, track: 6 },
+      { id: 84, disk: 1, track: 7 },
+      { id: 83, disk: 1, track: 8 },
+      { id: 82, disk: 1, track: 9 },
+      { id: 81, disk: 1, track: 10 },
+      { id: 80, disk: 1, track: 11 },
+      { id: 79, disk: 1, track: 12 }
+    ]
+  ],
+  [25, [{ id: 197, disk: 1, track: 1 }]],
+  [77, [{ id: 449, disk: 1, track: 1 }]],
+  [
+    100,
+    [
+      { id: 555, disk: 1, track: 1 },
+      { id: 558, disk: 1, track: 2 },
+      { id: 557, disk: 1, track: 3 },
+      { id: 556, disk: 1, track: 4 }
+    ]
+  ]
+]);
+
 test.each(indexes)('get %d details', async count => {
   const ids = albumIds.slice(0, count);
   const rows = await Albums.getDetails(ids);
 
   rows.forEach(row => {
     expect(row).toEqual(albumRows.get(row.id));
+  });
+});
+
+test('map artists', async () => {
+  const map = await Albums.mapArtists(albumIds);
+
+  map.forEach((value, key) => {
+    expect(value).toEqual(albumArtists.get(key));
+  });
+});
+
+test('map songs', async () => {
+  const map = await Albums.mapSongs(albumIds);
+
+  map.forEach((value, key) => {
+    expect(value).toEqual(albumSongs.get(key));
+  });
+});
+
+test.each(indexes)('add %d details', async count => {
+  const albums = albumIds.slice(0, count).map(a => ({ id: a }));
+  await Albums.addDetails(albums);
+
+  albums.forEach(album => {
+    expect(album).toEqual(albumRows.get(album.id));
+  });
+});
+
+test.each(indexes)('add %d artists', async count => {
+  const albums = albumIds.slice(0, count).map(a => ({ id: a }));
+  await Albums.addArtists(albums);
+
+  albums.forEach(album => {
+    expect(album.artists).toEqual(albumArtists.get(album.id));
   });
 });
--- server/db/albums.js
+++ server/db/albums.js
@@ -1,4 +1,5 @@
 const { query } = require('./query.js');
+const { mapBs } = require('./artists.js');
 
 const getDetails = async ids => {
   const sql = `
@@ -8,6 +9,85 @@ const getDetails = async ids => {
   return query(sql);
 };
 
+const addDetails = async albums => {
+  const rows = await getDetails(albums.map(a => a.id));
+  const map = new Map(albums.map(a => [a.id, a]));
+
+  rows.forEach(({ id, title, format, format2, release }) => {
+    map.get(id).title = title;
+    map.get(id).format = format;
+    map.get(id).format2 = format2;
+    map.get(id).release = release;
+  });
+};
+
+const mapArtists = async ids => {
+  const sql = `
+    SELECT AlbumId, \`order\`, ArtistId, name
+      FROM AlbumArtists a, Artists b
+     WHERE a.AlbumId in (${ids.join()})
+       AND a.ArtistId = b.id;`;
+  const rows = await query(sql);
+
+  const albumArtists = new Map();
+  const artists = new Set();
+
+  rows.forEach(({ AlbumId, order, ArtistId, name }) => {
+    if (!albumArtists.has(AlbumId)) {
+      albumArtists.set(AlbumId, []);
+    }
+
+    albumArtists.get(AlbumId)[order] = { id: ArtistId, name };
+    artists.add(ArtistId);
+  });
+
+  const bs = await mapBs([...artists]);
+  return new Map(
+    [...albumArtists].map(([key, value]) => [
+      key,
+      value.map(({ id, name }) => {
+        if (bs.has(id)) {
+          return { id, name, Bs: bs.get(id) };
+        }
+        return { id, name };
+      })
+    ])
+  );
+};
+
+const addArtists = async albums => {
+  const artists = await mapArtists(albums.map(a => a.id));
+  const map = new Map(albums.map(a => [a.id, a]));
+
+  artists.forEach((entry, id) => {
+    map.get(id).artists = entry;
+  });
+};
+
+const mapSongs = async ids => {
+  const sql = `
+    SELECT SongId, AlbumId, disk, track
+      FROM AlbumSongs a
+     WHERE a.AlbumId in (${ids.join()})
+  ORDER BY AlbumId, disk, track;`;
+  const rows = await query(sql);
+
+  const albumSongs = new Map();
+  rows.forEach(({ SongId, AlbumId, disk, track }) => {
+    if (!albumSongs.has(AlbumId)) {
+      albumSongs.set(AlbumId, []);
+    }
+
+    albumSongs.get(AlbumId).push({ id: SongId, disk, track });
+  });
+
+  return albumSongs;
+};
+
 module.exports = {
-  getDetails
+  addArtists,
+  addDetails,
+  getDetails,
+  mapArtists,
+  mapSongs
 };
