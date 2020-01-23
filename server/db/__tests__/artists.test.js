27f688cb7536516f5dae70735a51230968dfdf3f
eunmo <eunmo@cs.stanford.edu>
2020-01-23 14:18:48 +0000

    complete db/artists.js

--- server/db/__tests__/artists.test.js
+++ server/db/__tests__/artists.test.js
@@ -1,25 +1,43 @@
 const Artists = require('../artists.js');
 
-const indexes = [1, 2, 3];
-const artistIds = [1, 10, 100];
-const artistRows = new Map([
+const indexes = [1, 2, 3, 4];
+const artistIds = [1, 10, 100, 317];
+const artistNames = new Map([
   [1, { id: 1, name: 'Apink', nameNorm: 'Apink' }],
   [10, { id: 10, name: 'Zedd', nameNorm: 'Zedd' }],
-  [100, { id: 100, name: '10cm', nameNorm: '10cm' }]
+  [100, { id: 100, name: '10cm', nameNorm: '10cm' }],
+  [317, { id: 317, name: '정은지', nameNorm: '정은지' }]
 ]);
 
-const artistRows2 = new Map([
+const artistDetails = new Map([
   [1, { name: 'Apink', gender: 'Female', type: 'Group', origin: 'Korea' }],
   [10, { name: 'Zedd', gender: 'Male', type: 'Solo', origin: 'Russia' }],
-  [100, { name: '10cm', gender: 'Male', type: 'Duet', origin: 'Korea' }]
+  [100, { name: '10cm', gender: 'Male', type: 'Duet', origin: 'Korea' }],
+  [317, { name: '정은지', gender: 'Female', type: 'Solo', origin: 'Korea' }]
 ]);
 
-test.each(indexes)('get %d details', async count => {
+const artistAlbumsAndSongsCount = new Map([
+  [1, 49],
+  [2, 66],
+  [3, 89],
+  [4, 105]
+]);
+
+const artistAs = new Map([
+  ['1.317', { id: 317, type: 'm', order: null, name: '정은지' }],
+  ['100.769', { id: 769, type: 'p', order: 1, name: '센치한 하하' }],
+  ['100.798', { id: 798, type: 'm', order: null, name: '권정열' }]
+]);
+
+const artistBs = new Map([[317, new Map([['m', { id: 1, name: 'Apink' }]])]]);
+
+test.each(indexes)('get %d names', async count => {
   const ids = artistIds.slice(0, count);
   const rows = await Artists.getNames(ids);
 
+  expect(rows.length).toBe(count);
   rows.forEach(row => {
-    expect(row).toEqual(artistRows.get(row.id));
+    expect(row).toEqual(artistNames.get(row.id));
   });
 });
 
@@ -28,6 +46,38 @@ test.each(artistIds)('get %d details', async id => {
 
   expect(rows.length).toBe(1);
   rows.forEach(row => {
-    expect(row).toEqual(artistRows2.get(id));
+    expect(row).toEqual(artistDetails.get(id));
+  });
+});
+
+test.each(indexes)('get %d albums and songs', async count => {
+  const ids = artistIds.slice(0, count);
+  const rows = await Artists.getAlbumsAndSongs(ids);
+
+  expect(rows.length).toBe(artistAlbumsAndSongsCount.get(count));
+});
+
+test.each(artistIds)('get %d A', async id => {
+  const rows = await Artists.getA(id);
+
+  rows.forEach(row => {
+    expect(row).toEqual(artistAs.get(`${id}.${row.id}`));
+  });
+});
+
+test('map Bs', async () => {
+  const map = await Artists.mapBs(artistIds);
+
+  map.forEach((value, key) => {
+    expect(value).toEqual(artistBs.get(key));
+  });
+});
+
+test.each(indexes)('add %d names', async count => {
+  const artists = artistIds.slice(0, count).map(a => ({ id: a }));
+  await Artists.addNames(artists);
+
+  artists.forEach(artist => {
+    expect(artist).toEqual(artistNames.get(artist.id));
   });
 });
--- server/db/artists.js
+++ server/db/artists.js
@@ -16,7 +16,76 @@ const getDetail = async id => {
   return query(sql);
 };
 
+const getAlbumsAndSongs = ids => {
+  const sql = `
+    SELECT ArtistId, b.AlbumId, SongId, disk, track
+      FROM AlbumArtists a, AlbumSongs b
+     WHERE a.ArtistId in (${ids.join()})
+       AND a.AlbumId = b.AlbumId
+    UNION
+    SELECT ArtistId, AlbumId, a.SongId, disk, track
+      FROM SongArtists a, AlbumSongs b
+     WHERE a.ArtistId in (${ids.join()})
+       AND a.SongId = b.SongId;`;
+  return query(sql);
+};
+
+const getA = id => {
+  const sql = `
+    SELECT ar.\`type\`, ar.order, a.name, a.id
+      FROM ArtistRelations ar, Artists a
+     WHERE ar.b = ${id}
+       AND ar.a = a.id;`;
+  return query(sql);
+};
+
+const mapBs = async ids => {
+  const sql = `
+    SELECT ar.a, ar.b, ar.\`type\`, ar.order, a.name
+      FROM ArtistRelations ar, Artists a
+     WHERE ar.a in (${ids.join()})
+       AND ar.b = a.id;`;
+  const rows = await query(sql);
+  const Bs = new Map();
+
+  rows.forEach(({ a, b: id, type, order, name }) => {
+    const b = { id, name };
+
+    if (!Bs.has(a)) {
+      Bs.set(a, new Map());
+    }
+
+    const artist = Bs.get(a);
+
+    if (type !== 'p') {
+      artist.set(type, b);
+    } else {
+      // project group needs an order.
+      if (!artist.has(type)) {
+        artist.set(type, []);
+      }
+      artist.get(type)[order] = b;
+    }
+  });
+
+  return Bs;
+};
+
+const addNames = async artists => {
+  const rows = await getNames(artists.map(a => a.id));
+  const map = new Map(artists.map(a => [a.id, a]));
+
+  rows.forEach(({ id, name, nameNorm }) => {
+    map.get(id).name = name;
+    map.get(id).nameNorm = nameNorm;
+  });
+};
+
 module.exports = {
+  addNames,
+  getA,
+  getAlbumsAndSongs,
   getNames,
-  getDetail
+  getDetail,
+  mapBs
 };
