8a2ed1acb6df8d9e6cf156c0bd8196e11705d2a7
eunmo <eunmo@cs.stanford.edu>
2020-02-02 16:04:31 +0000

    add artist relation table dml

---
+++ server/db/dml/__tests__/artistRelation.test.js
@@ -0,0 +1,68 @@
+const { dml, query } = require('../dml.js');
+const { add, remove, update } = require('../artistRelation.js');
+
+jest.mock('../../db.json', () => {
+  return {
+    host: 'localhost',
+    user: 'music',
+    password: 'music',
+    database: 'musictest'
+  };
+});
+
+beforeAll(async () => {
+  await dml('DROP TABLE IF EXISTS ArtistRelations;');
+  await dml('CREATE TABLE ArtistRelations LIKE music.ArtistRelations;');
+});
+
+afterAll(async () => {
+  await dml('DROP TABLE IF EXISTS ArtistRelations;');
+});
+
+beforeEach(async () => {
+  await dml('TRUNCATE TABLE ArtistRelations;');
+  await dml(
+    "INSERT INTO ArtistRelations (type, `order`, A, B) VAlUES ('m', null, 1, 2);"
+  );
+
+  const rows = await query('SELECT * FROM ArtistRelations');
+  expect(rows.length).toBe(1);
+});
+
+test.each([
+  ['p', 0, 1],
+  ['m', null, 2]
+])('add', async (type, order, A) => {
+  await add(type, order, A, 3);
+
+  const rows = await query('SELECT * FROM ArtistRelations WHERE B=3');
+  expect(rows.length).toBe(1);
+
+  const row = rows[0];
+  expect(row.type).toBe(type);
+  expect(row.order).toBe(order);
+  expect(row.A).toBe(A);
+});
+
+test('remove', async () => {
+  await remove(1, 2);
+
+  const rows = await query('SELECT * FROM ArtistRelations');
+  expect(rows.length).toBe(0);
+});
+
+test.each([
+  ['p', 0],
+  ['a', null],
+  ['m', null]
+])('update', async (type, order) => {
+  const result = await update(type, order, 1, 2);
+  expect(result).toBe(type !== 'm' || order !== null);
+
+  const rows = await query('SELECT * FROM ArtistRelations');
+  expect(rows.length).toBe(1);
+
+  const row = rows[0];
+  expect(row.type).toBe(type);
+  expect(row.order).toBe(order);
+});
---
+++ server/db/dml/artistRelation.js
@@ -0,0 +1,31 @@
+const { dml } = require('./dml.js');
+
+const add = async (type, order, a, b) => {
+  return dml(`
+    INSERT INTO ArtistRelations (type, \`order\`, A, B)
+         VALUES ('${type}', ${order}, ${a}, ${b});`);
+};
+
+const remove = async (a, b) => {
+  return dml(`
+    DELETE FROM ArtistRelations
+     WHERE A=${a}
+       AND B=${b};`);
+};
+
+const update = async (type, order, a, b) => {
+  const result = await dml(`
+    UPDATE ArtistRelations
+       SET type='${type}',
+           \`order\`=${order}
+     WHERE A=${a}
+       AND B=${b};`);
+
+  return result.changedRows === 1;
+};
+
+module.exports = {
+  add,
+  remove,
+  update
+};
