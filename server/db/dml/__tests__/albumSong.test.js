a7a499ae914b458fdbc3d136ef8b9cab77a9c069
eunmo <eunmo@cs.stanford.edu>
2020-02-02 16:24:06 +0000

    add album song table dml

---
+++ server/db/dml/__tests__/albumSong.test.js
@@ -0,0 +1,33 @@
+const { dml, query } = require('../dml.js');
+const { add } = require('../albumSong.js');
+
+jest.mock('../../db.json', () => {
+  return {
+    host: 'localhost',
+    user: 'music',
+    password: 'music',
+    database: 'musictest'
+  };
+});
+
+beforeAll(async () => {
+  await dml('DROP TABLE IF EXISTS AlbumSongs;');
+  await dml('CREATE TABLE AlbumSongs LIKE music.AlbumSongs;');
+});
+
+afterAll(async () => {
+  await dml('DROP TABLE IF EXISTS AlbumSongs;');
+});
+
+test.each([[1, 1, 1, 1]])('add', async (disk, track, song, album) => {
+  await add(disk, track, song, album);
+
+  const rows = await query('SELECT * FROM AlbumSongs');
+  expect(rows.length).toBe(1);
+
+  const row = rows[0];
+  expect(row.disk).toBe(disk);
+  expect(row.track).toBe(track);
+  expect(row.SongId).toBe(song);
+  expect(row.AlbumId).toBe(album);
+});
---
+++ server/db/dml/albumSong.js
@@ -0,0 +1,11 @@
+const { dml } = require('./dml.js');
+
+const add = async (disk, track, songId, albumId) => {
+  return dml(`
+    INSERT INTO AlbumSongs (disk, track, SongId, AlbumId)
+         VALUES (${disk}, ${track}, ${songId}, ${albumId});`);
+};
+
+module.exports = {
+  add
+};
